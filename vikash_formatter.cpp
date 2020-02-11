75115160adee9e4870721c44e72073be9a28eb30
Vikash Kumar Choubey <Vikash.2488@gmail.com>
2020-02-11 21:34:18 +0530

    Handling corner cases related to string to be replace.
    Handling case where second agrument is not given or number of strings to be replace are less than number string options provided.

--- vikash_formatter.cpp
+++ vikash_formatter.cpp
@@ -19,27 +19,34 @@ vector<std::string> split(const std::string& s, char delimiter)
 }
 
 
-string get_replaced_string(string format_string, vector<string> list_of_parameters)
+string pseudo_string_formatter(string format_string, vector<string> list_of_parameters)
 {
 	int idx_number;
 	int format_string_len = format_string.length();
+	int num_of_string_to_replace = list_of_parameters.size();
+
 	string final_string;
+
+
 	for( int idx = 0; idx < format_string_len; idx ++)
 	{
+		bool replace_character = true;
 		if(format_string[idx] == '{')
 		{
 			if(format_string[idx + 2] == '}')
 			{
 				idx_number = (int)format_string[idx + 1] - 48;
-				final_string.append(list_of_parameters[idx_number].c_str());
-				idx += 2;
-			}
-			else
-			{
-				final_string.push_back(format_string[idx]);
+
+				if(idx_number < num_of_string_to_replace)
+				{
+					final_string.append(list_of_parameters[idx_number].c_str());
+					idx += 2;
+					replace_character = false;
+				}
 			}
 		}
-		else
+		
+		if(replace_character)
 		{
 			final_string.push_back(format_string[idx]);
 		}
@@ -48,11 +55,18 @@ string get_replaced_string(string format_string, vector<string> list_of_paramete
 }
 
 
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-    vector<string> list_replace_str = split(argv[2], ',');
+	vector<string> list_replace_str;
 
-    cout << get_replaced_string(argv[1], list_replace_str) << endl;
+	if(argc == 3)
+	{
+	    list_replace_str = split(argv[2], ',');
+	}
+
+	string formatted_string = pseudo_string_formatter(argv[1], list_replace_str);
+
+	cout << formatted_string << endl;
 
     return 0; 
 }
