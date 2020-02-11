c66b004aca1d0e6dfe19d4cb844708122123b4fd
Vikash Kumar Choubey <Vikash.2488@gmail.com>
2020-02-11 21:18:53 +0530

    Compiled code with string splits.

---
+++ vikash_formatter.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std; 
+
+
+vector<std::string> split(const std::string& s, char delimiter)
+{
+   vector<string> tokens;
+   string token;
+   istringstream tokenStream(s);
+   while (std::getline(tokenStream, token, delimiter))
+   {
+      tokens.push_back(token);
+   }
+   return tokens;
+}
+
+
+string get_replaced_string(string format_string, vector<string> list_of_parameters)
+{
+	int idx_number;
+	int format_string_len = format_string.length();
+	string final_string;
+	for( int idx = 0; idx < format_string_len; idx ++)
+	{
+		if(format_string[idx] == '{')
+		{
+			if(format_string[idx + 2] == '}')
+			{
+				idx_number = (int)format_string[idx + 1] - 48;
+				final_string.append(list_of_parameters[idx_number].c_str());
+			}
+			else
+			{
+				final_string.push_back(format_string[idx]);
+			}
+		}
+		else
+		{
+			final_string.push_back(format_string[idx]);
+		}
+	}
+	return final_string;
+}
+
+
+int main(int argc, char** argv) 
+{
+    vector<string> list_replace_str = split(argv[2], ',');
+    cout << list_replace_str.size() << endl;
+
+    vector<string>::iterator itr;
+    for (itr = list_replace_str.begin(); itr != list_replace_str.end(); ++itr)
+    {
+        cout << *itr << endl;
+    }
+
+    cout << get_replaced_string(argv[1], list_replace_str) << endl;
+
+
+    return 0; 
+}
